b4b2543ce65329a5d3f863242056a52ddd81297c
Rafael Custódio <rafael.cdpaula@gmail.com>
2022-02-05 21:46:54 +0100

    adding testing

---
+++ tests/API/UsersTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\API;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UsersTest extends WebTestCase
+{
+    public function testGetUsers()
+    {
+        $client = static::createClient();
+        $client->request("GET", "/api/users");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateError()
+    {
+        $client = static::createClient();
+        $client->jsonRequest("POST", "/api/users");
+        $this->assertEquals(422, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateEmailError()
+    {
+        $client = static::createClient();
+        $client->jsonRequest("POST", "/api/users", [
+            'email' => "rafael",
+            'firstName' => "Rafael",
+            "lastName" => "Custodio"
+        ]);
+        $this->assertEquals(422, $client->getResponse()->getStatusCode());
+        $json = json_decode($client->getResponse()->getContent(), JSON_OBJECT_AS_ARRAY);
+        $this->assertCount(1, $json['violations']);
+        $this->assertEquals('email', $json['violations'][0]['propertyPath']);
+    }
+
+    public function testGetNotFoundUser()
+    {
+        $client = static::createClient();
+        $client->request("GET", "/api/users/0");
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+    }
+}
